3cc09d7dea5b4b6bb5e705e445a6e13c2ed1e43a
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-03-06 16:36:39 +0100

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        evento: {
+          100: "#6e8df5",
+          200: "#250c41",
+          300: "#9cbe60",
+          400: "#cada97",
+          500: "#8ddd99",
+          600: "#95a1ed",
+          700: "#4e4fb0",
+          800: "#546cb4",
+          900: "#188697",
+        },
+      },
     },
-    plugins: [],
+  },
+  plugins: ["@tailwindcss/forms"],
 };
-
