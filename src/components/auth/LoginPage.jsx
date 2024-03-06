04100b5aebec41ccba83af129a11b3b31b97ad64
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-03-06 16:44:31 +0100

    clean up

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -29,6 +29,7 @@ const LoginPage = () => {
           <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
               <form className="space-y-6" onSubmit={handleSubmit} method="POST">
+                
                 <div>
                   <Label htmlFor="email">Email address</Label>
                   <Input
