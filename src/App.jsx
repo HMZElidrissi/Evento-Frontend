dd8a0bb58a244eeebb08ea388323afe70d8712ac
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-03-07 10:56:28 +0100

    home page component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import LoginPage from "./components/auth/LoginPage";
 import RegisterPage from "./components/auth/RegisterPage";
 import ForgetPasswordPage from "./components/auth/ForgetPasswordPage";
 import DashboardPage from "./components/backOffice/DashboardPage";
+import HomePage from "./components/frontOffice/HomePage";
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgetPasswordPage />} />
