4d5ea22a9788d552a34d8eaf15dda997ad07d965
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-03-08 14:35:21 +0100

    users page

--- src/components/backOffice/UsersPage.jsx
+++ src/components/backOffice/UsersPage.jsx
@@ -1,9 +1,119 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../../axios-client";
+
 const UsersPage = () => {
-    return (
-        <div>
-            <h1>Users</h1>
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const revokeAccess = (userId) => {
+    axiosClient.put(`/users/${userId}/revoke-access`).then(() => {
+      getUsers();
+    });
+  };
+
+  const getUsers = () => {
+    setLoading(true);
+    axiosClient
+      .get("/users")
+      .then((response) => {
+        setLoading(false);
+        console.log(response);
+        setUsers(response.data);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="overflow-hidden flex flex-col">
+      <div className="-my-2 sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Role
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Revoke</span>
+                  </th>
+                </tr>
+              </thead>
+              {loading && (
+                <tbody>
+                  <tr>
+                    <td colSpan="5" className="text-center">
+                      Loading...
+                    </td>
+                  </tr>
+                </tbody>
+              )}
+              {!loading && (
+                <tbody>
+                  {users.map((person, personIdx) => (
+                    <tr
+                      key={person.id}
+                      className={
+                        personIdx % 2 === 0 ? "bg-white" : "bg-gray-50"
+                      }
+                    >
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {person.name}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {person.email}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {person.role.role}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {person.is_active ? "Active" : "Inactive"}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <button
+                          onClick={() => revokeAccess(person.id)}
+                          className="text-evento-600 hover:text-evento-900"
+                        >
+                          Revoke Access
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
+            </table>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UsersPage;
